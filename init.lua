310078fcc6c8298ea74d2caf8131831b9bb2b3eb
Tarcisio Brito <tarcisiofbr@gmail.com>
2024-10-27 13:18:11 -0300

    fixed autocompletion for python and r

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+-- Setups
 require("config.lazy")
 require('mason').setup()
 require('mini.surround').setup()
@@ -7,10 +8,14 @@ require('mason-lspconfig').setup({
 require'lspconfig'.r_language_server.setup{}
 require'lspconfig'.pylsp.setup{}
 require("nvim-treesitter.configs").setup({
-            ensure_installed = { "markdown", "markdown_inline", "r", "rnoweb", "yaml", "python", "awk", "bash"},
-            highlight = { enable = true },
-        })
+    ensure_installed = { "markdown", "markdown_inline", "r", "rnoweb", "yaml", "python", "awk", "bash"},
+    highlight = { enable = true },
+})
 
+
+-- Test cmp
+
+-- Cmds
 vim.cmd("set colorcolumn=80")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set expandtab")
@@ -20,15 +25,14 @@ vim.cmd("set scrolloff=10")
 vim.cmd("set scroll=10")
 vim.cmd("colorscheme catppuccin")
 
--- # Telescope
+-- Telescope
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
--- # My keymaps
-
+-- My keymaps
 vim.keymap.set('i', '<M-->', ' <- ')
 vim.keymap.set('i', '<M-m>', ' %>% ')
 vim.keymap.set('i', '<M-i>', ' %in% ')
@@ -40,3 +44,80 @@ vim.keymap.set("n", "<A-k>", ":m .-2<CR>==") -- move line down(n)
 vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv") -- move line up(v)
 vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv") -- move line down(v)
 
+vim.keymap.set("n", "<leader>e", ":e ") -- move line down(v)
+vim.keymap.set("n", "<leader>bd", ":bd<CR>") -- move line down(v)
+
+
+-- Set up nvim-cmp.
+local cmp = require'cmp'
+
+cmp.setup({
+snippet = {
+  -- REQUIRED - you must specify a snippet engine
+  expand = function(args)
+    -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+    -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+    require('snippy').expand_snippet(args.body) -- For `snippy` users.
+    -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+    -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+  end,
+},
+window = {
+  -- completion = cmp.config.window.bordered(),
+  -- documentation = cmp.config.window.bordered(),
+},
+mapping = cmp.mapping.preset.insert({
+  ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+  ['<C-f>'] = cmp.mapping.scroll_docs(4),
+  ['<C-Space>'] = cmp.mapping.complete(),
+  ['<C-e>'] = cmp.mapping.abort(),
+  ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+}),
+sources = cmp.config.sources({
+  { name = 'snippy' }, -- For snippy users.
+  { name = 'cmp_nvim_r' },
+  { name = 'nvim_lsp' },
+  { name = 'vsnip' }, -- For vsnip users.
+  -- { name = 'luasnip' }, -- For luasnip users.
+  -- { name = 'ultisnips' }, -- For ultisnips users.
+}, {
+  { name = 'buffer' },
+})
+})
+
+-- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
+-- Set configuration for specific filetype.
+--[[ cmp.setup.filetype('gitcommit', {
+sources = cmp.config.sources({
+  { name = 'git' },
+}, {
+  { name = 'buffer' },
+})
+})
+require("cmp_git").setup() ]]-- 
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+mapping = cmp.mapping.preset.cmdline(),
+sources = {
+  { name = 'buffer' }
+}
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+mapping = cmp.mapping.preset.cmdline(),
+sources = cmp.config.sources({
+  { name = 'path' }
+}, {
+  { name = 'cmdline' }
+}),
+matching = { disallow_symbol_nonprefix_matching = false }
+})
+
+-- Set up lspconfig.
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+require('lspconfig')['pylsp'].setup {
+capabilities = capabilities
+}
